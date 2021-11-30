87af263f8fb618b543f16ac871f8466a753ec48a
Benjamin <dotsehyde@gmail.com>
2021-11-30 16:13:49 +0000

    Update configs

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import VueSmoothScroll from "vue2-smooth-scroll";
+
+Vue.use(VueSmoothScroll);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
